b926b1a95432897588927604aeace503e90151da
fauzan18296 <fauzan18296@gmail.com>
2024-02-26 23:16:35 +0700

    day 11,12,13,14

---
+++ JS/12-convertStringToNumber!.js
@@ -0,0 +1,3 @@
+// Algoritma: Jadi type data string yang berupa number text diubah menjadi tipe data Number dengan cara diuraikan menjadi type data number. 
+const stringToNumber = (str) => parseFloat(str);
+console.log(stringToNumber("123"));
