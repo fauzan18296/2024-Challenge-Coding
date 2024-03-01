36190a8d350e1348e0626a0052fb453952d79591
fauzan18296 <fauzan18296@gmail.com>
2024-03-01 17:44:46 +0700

    add(day): 15

--- JS/15-countingSheeo.js
+++ JS/15-countingSheeo.js
@@ -1,21 +1,19 @@
 const countSheeps = (sheep) => {
- // Algoritma
- let s = [];
- //  1. Mulai dengan statement perulangan untuk  mencari jumlah atau panjang dari tipe data true(benar).
- for(let i = 0; i<sheep.length; i++){
-  // 2. Jika sheep dari index ke 0 bernilai true(benar).
-   if(sheep) {
-    // 3. Maka manipulasi data dari sheep dengan method filter dan method length untuk menghitung length yang berisi nilai true(benar).
-   return sheep.filter((el) => el == true).length;
- }else {
-  // 3. Jika false(salah) dan error, maka keluarkan hasil atau output yang bernilai undefined atau null.
-   return undefined;
+  // Algoritma
+  //  1. Mulai dengan statement perulangan untuk  mencari jumlah atau panjang dari tipe data true(benar).
+  for(let i = 0; i<sheep.length; i++){
+   // 2. Jika sheep dari index ke 0 bernilai true(benar).
+    if(sheep) {
+     // 3. Maka manipulasi data dari sheep dengan method filter dan method length untuk menghitung length yang berisi nilai true(benar).
+    return sheep.filter((el) => el == true).length;
+  }else {
+   // 4. Jika false(salah) dan error, maka keluarkan hasil atau output yang bernilai undefined atau null.
+    return undefined;
+  }
  }
-}
-// 4. Setelah itu return 0 sebagai nilai default saat tidak ada nilai yang benar.
-return 0;
-};
-
+ // 5. Setelah itu return 0 sebagai nilai default saat tidak ada nilai yang benar.
+ return 0;
+ };
 console.log(countSheeps([true,  true,  true,  false,
  true,  true,  true,  true ,
  true,  false, true,  false,
