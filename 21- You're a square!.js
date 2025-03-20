b75beb24a1641cb5d44e04b64ffe62ddae120768
Ahmad Fauzan <fauzan18296@gmail.com>
2025-03-21 00:49:04 +0700

    delete

--- 21- You're a square!.js
+++
@@ -1,8 +0,0 @@
-let isSquare = function (n) {
-  return Math.sqrt(n) % 1 === 0 ? true : false;
-}
-
-console.log(isSquare(-1))
-console.log(isSquare(0))
-console.log(isSquare(3))
-console.log(isSquare(4))
