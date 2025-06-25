a7c6f2bb81abc82aeb6185a72aebfc6ded93f650
Ahmad Fauzan <fauzan18296@gmail.com>
2025-06-25 16:09:55 +0700

    refactor: +Refactor only Question coding challenge in codeWars level 6 kyu

--- JS/77-isValidWalk.js
+++ JS/77-isValidWalk.js
@@ -1,5 +1,11 @@
 function isValidWalk(walk) {
   // Objective => Mencari jalan yang hanya butuh 10 menit saja dan Mencari jumlah jalan yang sama
+  // Remember: for in sangat tidak disarankan untuk iterasi yang berhubungan dengan data array, for in lebih disarankan untuk suatu data yang tipe nya object atau array object
+
+  // Jika jalan yang diambil tidak sama dengan 10 menit, maka jalan tersebut tidak valid atau false
+  if (walk.length != 10) {
+        return false
+    }
   let result = {}
   for (const key of walk) {
     // Mencari jumlah jalan yang diambil dan akan dijadikan sebagai nilai dari objek result
@@ -8,11 +14,8 @@ function isValidWalk(walk) {
 
   for (const freq in result) {
     console.log(result['n'] == result['s'] && result['e'] == result['w']);
-    // Jika jalan yang diambil tidak sama dengan 10 menit, maka jalan tersebut tidak valid atau false
-    if (walk.length != 10) {
-      return false
-    } // Jika jalan yang diambil sama dengan 10 menit, dan jumlah jalan yang dimiliki sama maka jalan tersebut valid atau true
-    else if (result['n'] == result['s'] && result['e'] == result['w']) {
+    // Jika jalan yang diambil sama dengan 10 menit, dan jumlah jalan yang dimiliki sama maka jalan tersebut valid atau true
+     if (result['n'] == result['s'] && result['e'] == result['w']) {
       return true
     } else {
       return false
